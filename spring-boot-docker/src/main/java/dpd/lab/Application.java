4b45b409f91a2246a8df7e20d6754075b51199e7
Dominic Diamante <ddiamante@whispir.com>
2021-09-25 00:43:45 +1000

    Some basic hygene. java.util.Date isn't used.

--- spring-boot-docker/src/main/java/dpd/lab/Application.java
+++ spring-boot-docker/src/main/java/dpd/lab/Application.java
@@ -1,6 +1,5 @@
 package dpd.lab;
 
-import java.util.Date;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
